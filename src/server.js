82379690290549a094f6bae8db61eb98822bae54
Bright Iheawgam <bright06iheagwam@gmail.com>
2025-09-18 05:58:08 +0100

    setup database connection

--- src/server.js
+++ src/server.js
@@ -1,3 +1,19 @@
+import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import cors from "cors";
+dotenv.config();
 
-dotenv.config(); // loads variables from .env into process.env
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// connect to database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Admin API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
